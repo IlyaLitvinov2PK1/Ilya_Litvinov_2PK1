12e438fed25aee07898e17ec2ba2c94138b64b58
Илья Геннадьевич Литвинов <tanya.litvinova.1977@list.ru>
2021-10-15 12:23:59 +0500

    Добавьте файлы проекта.

---
+++ Ilya_Litvinov_2PK1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ilya_Litvinov_2PK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const int size = 10;
+            const int x = -1000;
+            const int y = 1000;
+            int counter = 0;
+            int[] arr = new int[size];
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rand.Next(x, y);
+                if (arr[i] < 0)
+                    counter++;
+                Console.WriteLine((i + 1) + ") " + arr[i]);
+            }
+            Console.WriteLine("\nКоличество отрицательных чисел: " + counter); // извините, не могу вывести все отрицательные числа в порядке уменьшения, только кол-во
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pz_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const int size = 10;
+            const int x = -100;
+            const int y = 100;
+            int counter = 0;
+            int[] arr = new int[size];
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rand.Next(x, y);
+                if (arr[i] < 0)
+                    counter++;
+                Console.WriteLine((i + 1) + ") " + arr[i]);
+            }
+            Console.WriteLine("\nКоличество отрицательных элементов: " + counter); // не получается вывести все отрицательные числа в порядке возрастания, извините :(
+            Console.ReadKey();
+
+        }
+    }
+}
+        
+    
+
