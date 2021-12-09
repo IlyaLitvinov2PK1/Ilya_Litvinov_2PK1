a9b93627a74326b83c39d229d0821c27dd7f9e53
Lexa Popov <papov97@inbox.ru>
2021-12-09 08:57:19 +0500

    pz

---
+++ pz_16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pz_16
+{
+    class Program
+    {
+
+        static void pitsa(double N)
+        {
+
+            double SO = (System.Math.Pow(N, 2)) * System.Math.PI;
+            double VS = (System.Math.Pow(N, 3)) * System.Math.PI * ((double)4 / 3);
+            double SS = (System.Math.Pow(N, 2)) * System.Math.PI * 4;
+            Console.WriteLine($"Площадь окружности: {SO}\nобъем сферы: {VS}\nплощадь сферы: {SS}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус:");
+            pitsa(Convert.ToDouble(Console.ReadLine()));
+
+        }
+    }
+
+}
